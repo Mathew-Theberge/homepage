386be4d5c8df7e5c8e215fa7f23204d6de60d9d8
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2025-03-31 11:11:39 -0400

    Add footer markup & add in project card photos & links & add load screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./style.css";
+
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", () => {
+    loader.classList.add("loader-hidden");
+    setTimeout(() => {
+        document.body.removeChild(loader);
+    }, 500);
+});
